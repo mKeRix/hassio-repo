222bd03bdeb891fe6eb18aa1cfa5fd04434f5d93
Heiko Rothe <me@heikorothe.com>
2020-02-20 12:57:28 +0100

    Add add-on for room-assistant beta

---
+++ room_assistant_beta/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 2.0.0-beta.6
+
+- Upgrades room-assistant to [2.0.0-beta.6](https://github.com/mKeRix/room-assistant/releases/tag/v2.0.0-beta.6)
---
+++ room_assistant_beta/rootfs/room-assistant/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bashio
+MQTT_CONFIG=$(bashio::services 'mqtt')
+if bashio::var.has_value "${MQTT_CONFIG}"; then
+    bashio::log.info "Setting up Home Assistant configuration"
+    echo "${MQTT_CONFIG}" | jq '{homeAssistant: {mqttUrl: "mqtt://\(.host):\(.port)", mqttOptions: {username: .username, password: .password}}}' > /room-assistant/config/default.json
+fi
+
+bashio::log.info "Starting room-assistant"
+exec room-assistant
